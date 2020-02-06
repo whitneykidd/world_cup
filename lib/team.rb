bd89de9f401542d311300626882b59ea5549211f
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-05 20:05:53 -0700

    Add add_player method and pass test_it_can_add_players

--- lib/team.rb
+++ lib/team.rb
@@ -1,17 +1,21 @@
 class Team
-  attr_reader :country, :eliminated
+  attr_reader :country, :eliminated, :players
 
   def initialize(country, eliminated = false)
     @country = country
     @eliminated = eliminated
+    @players = []
   end
 
   def eliminated?
     @eliminated
   end
 
-
   def eliminated
     @eliminated = true
+  end
+
+  def add_player(player)
+    @players << player
   end
 end
--- test/team_test.rb
+++ test/team_test.rb
@@ -22,4 +22,19 @@ class TeamTest < Minitest::Test
     assert_equal true, team.eliminated?
   end
 
+  def test_it_starts_without_players
+    team = Team.new("France")
+    assert_equal [], team.players
+  end
+
+  def test_it_can_add_players
+    team = Team.new("France")
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+
+    team.add_player(mbappe)
+    team.add_player(pogba)
+    assert_equal [mbappe, pogba], team.players
+  end
+
 end
