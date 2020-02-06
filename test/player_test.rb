a684c9abfd9ae81dc288b39a966b9e0e5230a959
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-05 19:48:42 -0700

    Actually complete iteration 1

--- test/player_test.rb
+++ test/player_test.rb
@@ -5,7 +5,16 @@ require './lib/player'
 class PlayerTest < Minitest::Test
   def test_it_exists
     player = Player.new({name: "Luka Modric", position: "midfielder"})
-    require "pry"; binding.pry
     assert_instance_of Player, player
+  end
+
+  def test_it_has_a_name
+    player = Player.new({name: "Luka Modric", position: "midfielder"})
+    assert_equal "Luka Modric", player.name
+  end
+
+  def test_it_has_a_position
+    player = Player.new({name: "Luka Modric", position: "midfielder"})
+    assert_equal "midfielder", player.position
   end
 end
